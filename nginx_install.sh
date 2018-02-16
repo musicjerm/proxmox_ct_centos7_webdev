35b6f5ddaff0d84c730fd2e2d54ef9c2a62a7de7
musicjerm <musicjerm@gmail.com>
2018-02-16 07:09:45 -0700

    fix nginx repo

--- nginx_install.sh
+++ nginx_install.sh
@@ -10,7 +10,7 @@ yum -y groupinstall "Development Tools"
 echo "# Nginx repo for Centos 7" > /etc/yum.repos.d/nginx.repo
 echo "[nginx]" >> /etc/yum.repos.d/nginx.repo
 echo "name=nginx repo" >> /etc/yum.repos.d/nginx.repo
-echo "baseurl=http://nginx.org/packages/centos/y/\$basearch/" >> /etc/yum.repos.d/nginx.repo
+echo "baseurl=http://nginx.org/packages/centos/7/\$basearch/" >> /etc/yum.repos.d/nginx.repo
 echo "gpgcheck=0" >> /etc/yum.repos.d/nginx.repo
 echo "enabled=1" >> /etc/yum.repos.d/nginx.repo
 
