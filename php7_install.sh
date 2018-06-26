ac7029fb931df550aaa2274ce9e5da6b849a3341
musicjerm <musicjerm@users.noreply.github.com>
2018-06-26 10:07:17 -0600

    Update php7_install.sh

--- php7_install.sh
+++ php7_install.sh
@@ -5,7 +5,7 @@ yum -y update
 
 # set version of php to install
 # check opcache extension directory if you change this
-phpVersion='7.2.4'
+phpVersion='7.2.7'
 
 # install dependencies
 yum -y groupinstall "Development Tools"
